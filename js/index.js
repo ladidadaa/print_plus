17c25f0ba996f1814801cdae647a01a27f9fa0b3
lad <ladimir.fil@yandex.ru>
2022-12-11 17:52:13 +0300

    registration

--- js/index.js
+++ js/index.js
@@ -1,13 +1,5 @@
 let current = undefined
 
-// function changeTab(){
-//     for (let i = 0; i < tabs.length; ++i){
-//         const tab = tabs[i]
-//         if (current.classList[1] == tab.classList[1]){
-//             return tab
-//         }
-//     }
-// }
 function changeTab(current){
     return tab = document.getElementsByClassName(current.classList[1])[1]
 }
@@ -19,15 +11,6 @@ function onMouseover() {
     current = this
     changeTab(current).hidden = false
 }
-
-// const items = document.getElementsByClassName('nav__element')
-// for (let i = 0; i < items.length; ++i) {
-//   const item = items[i]
-//   item.addEventListener('mouseover', onMouseover)
-// }
-// tabs = [document.getElementsByClassName('documents')[1], document.getElementsByClassName('photo')[1], 
-//         document.getElementsByClassName('document_photo')[1], document.getElementsByClassName('vizitki')[1], 
-//         document.getElementsByClassName('polk')[1]]
 
 elements = [document.getElementsByClassName('documents')[0], document.getElementsByClassName('photo')[0], 
             document.getElementsByClassName('document_photo')[0], document.getElementsByClassName('vizitki')[0], 
